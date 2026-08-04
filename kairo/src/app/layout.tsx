afc1b5f39f7abeff62ced304495182c2b225f7b5
kaycee <kaytech3570@gmail.com>
2026-08-04 21:55:54 +0100

    Update layout.tsx

--- kairo/src/app/layout.tsx
+++ kairo/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/ui/Header";
+import { Footer } from "@/components/ui/Footer";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
